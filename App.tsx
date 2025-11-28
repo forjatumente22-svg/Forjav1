00de31b7f10c66bb85709ca06b2147a6230a8ce2
José Antonio Durá Pastor <forjatumente22@gmail.com>
2025-11-28 17:22:04 +0100

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen w-full bg-black flex items-center justify-center p-4">
+      <h1 className="text-white text-4xl md:text-6xl lg:text-8xl font-black tracking-tighter text-center select-none animate-pulse">
+        FORJA TU MENTE:
+        <br />
+        <span className="text-gray-300">SISTEMA ONLINE</span>
+      </h1>
+    </div>
+  );
+};
+
+export default App;
